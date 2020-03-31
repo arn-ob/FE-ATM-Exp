4e662847430c7f376faf2524c37bfcc50ab48a94
Arnob Almazee <arnobxtreme@gmail.com>
2020-03-31 13:33:07 +0600

    test

--- script/script.js
+++ script/script.js
@@ -1,27 +1,21 @@
 $("#bookClick").click(() => {
     console.log("Clicked")
 
+    ga('test.ecommerce:addItem', {
+        'id': '1234',                     // Transaction ID. Required.
+        'name': 'Fluffy Pink Bunnies',    // Product name. Required.
+        'sku': 'DD23444',                 // SKU/code.
+        'category': 'Party Toys',         // Category or variation.
+        'price': '11.99',                 // Unit price.
+        'quantity': '1'                   // Quantity.
+      });
 
-    ga('send', {
-        hitType: 'event',
-        eventCategory: 'Videos',
-        eventAction: 'play',
-        eventLabel: 'Fall Campaign'
-    });
-
-    ga('send', {
-        hitType: 'event',
-        eventCategory: 'Videos',
-        eventAction: 'play',
-        eventLabel: 'Fall Campaign'
-    });
-
-    ga('ecommerce:addTransaction', {
+    ga('test.ecommerce:addTransaction', {
         'id': '12123123434',                     // Transaction ID. Required.
         'affiliation': 'Acmes Clothing',   // Affiliation or store name.
         'revenue': '11.99',               // Grand Total.
         'shipping': '5',                  // Shipping.
         'tax': '1.29'                     // Tax.
     });
-    ga('ecommerce:send');
+    ga('test.ecommerce:send');
 })
