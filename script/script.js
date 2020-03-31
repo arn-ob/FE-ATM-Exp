a3c862a098a2c78cb45573b9faa4fb546efc1f13
Arnob Almazee <arnobxtreme@gmail.com>
2020-03-31 13:48:11 +0600

    update

--- script/script.js
+++ script/script.js
@@ -1,8 +1,19 @@
 $("#bookClick").click(() => {
     console.log("Clicked")
 
+    ga('ecommerce:addItem', {
+        'id': '12asd2234',
+        'name': 'Test Test',
+        'sku': 'DD2344224',
+        'category': 'Party Tosys',
+        'price': '223',
+        'quantity': '1',
+        'currency': 'BDT' // local currency code.
+    });
+    ga('test.ecommerce:send');
+
     ga('test.ecommerce:addTransaction', {
-        'id': '12123123434',                     // Transaction ID. Required.
+        'id': '33345',                     // Transaction ID. Required.
         'affiliation': 'Acmes Clothing',   // Affiliation or store name.
         'revenue': '11.99',               // Grand Total.
         'shipping': '5',                  // Shipping.
