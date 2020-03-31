12d1c1c589c7d7f9fb214b56c121ff01b80b60b4
Arnob Almazee <arnobxtreme@gmail.com>
2020-03-31 15:01:47 +0600

    updae

--- script/script.js
+++ script/script.js
@@ -1,11 +1,20 @@
 $("#bookClick").click(() => {
-    console.log("Clicked")
-    ga('ec:setAction', 'purchase', {
-        id: 'asddsad232323',
-        affiliation: 'Google Store - Online',
-        revenue: '28.03',
-        tax: '2.14',
-        shipping: '4.00',
-    });
+    // console.log("Clicked")
+    // ga('ec:setAction', 'purchase', {
+    //     id: 'asddsad232323',
+    //     affiliation: 'Google Store - Online',
+    //     revenue: '28.03',
+    //     tax: '2.14',
+    //     shipping: '4.00',
+    // });
+    ga('ec:addProduct', {
+        'id': 'P12345',
+        'name': 'Android Warhol T-Shirt',
+        'category': 'Apparel',
+        'brand': 'Google',
+        'variant': 'black',
+        'position': 1
+      });
+      ga('ec:setAction', 'click', {list: 'Search Results'});
     ga('send', 'pageview');
 })
