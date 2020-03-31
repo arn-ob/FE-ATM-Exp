0589d3866ab713135dc7e06b5f05fdd15ca580e0
Arnob Almazee <arnobxtreme@gmail.com>
2020-03-31 13:23:57 +0600

    update

--- script/script.js
+++ script/script.js
@@ -9,5 +9,19 @@ $("#bookClick").click(() => {
         eventLabel: 'Fall Campaign'
     });
 
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'Videos',
+        eventAction: 'play',
+        eventLabel: 'Fall Campaign'
+    });
 
+    ga('ecommerce:addTransaction', {
+        'id': '1234',                     // Transaction ID. Required.
+        'affiliation': 'Acme Clothing',   // Affiliation or store name.
+        'revenue': '11.99',               // Grand Total.
+        'shipping': '5',                  // Shipping.
+        'tax': '1.29'                     // Tax.
+    });
+    ga('ecommerce:send');
 })
