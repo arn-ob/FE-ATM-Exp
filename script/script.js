4c8610fc3a3c21676007fa1172cad32a01cce1b7
Arnob Almazee <arnobxtreme@gmail.com>
2020-03-31 14:58:44 +0600

    update

--- script/script.js
+++ script/script.js
@@ -1,12 +1,11 @@
 $("#bookClick").click(() => {
     console.log("Clicked")
     ga('ec:setAction', 'purchase', {
-        id: 'T12345',
+        id: 'asddsad232323',
         affiliation: 'Google Store - Online',
         revenue: '28.03',
         tax: '2.14',
         shipping: '4.00',
-        coupon: 'SUMMER2013'
     });
     ga('send', 'pageview');
 })
